259eb4f0f8e9ff185b858a9d844f9e43b58c5864
GraphTechKEN <81954879+GraphTechKEN@users.noreply.github.com>
2026-09-23 18:25:57 +0900

    Add files via upload

---
+++ SELDController/SettingDefinition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SELDController
+{
+    internal class SettingDefinition
+    {
+        public int Address { get; set; }
+        public TextBox TextBox { get; set; }
+        public string Name { get; set; }
+    }
+}
